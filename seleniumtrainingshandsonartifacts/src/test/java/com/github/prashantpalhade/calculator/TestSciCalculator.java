9b8342d60388ea6a85d2a779785474cbfa66939e
PrashantPalhade <47560889+PrashantPalhade@users.noreply.github.com>
2019-06-19 19:35:13 +0530

    Update TestSciCalculator.java

--- seleniumtrainingshandsonartifacts/src/test/java/com/github/prashantpalhade/calculator/TestSciCalculator.java
+++ seleniumtrainingshandsonartifacts/src/test/java/com/github/prashantpalhade/calculator/TestSciCalculator.java
@@ -8,14 +8,39 @@ public class TestSciCalculator {
     void testSqrRoot() {
         ScientificCalculator sciCalObj = new ScientificCalculator();
         int result = sciCalObj.sqrRoot(9);
-
         Assert.assertEquals(result,3);
     }
     @Test
     void testcube() {
         ScientificCalculator sciCalObj = new ScientificCalculator();
         int result = sciCalObj.cube(9);
-        Assert.assertEquals(result,3);
-}
+        Assert.assertEquals(result,729);
+    }
+    
+    @Test
+    void testAddition() {
+        ScientificCalculator sciCalObj = new ScientificCalculator();
+        int result = sciCalObj.addition(10,12);
+        Assert.assertEquals(result,22);
+    }
+    
+    @Test
+    void testSubstract() {
+        ScientificCalculator sciCalObj = new ScientificCalculator();
+        int result = sciCalObj.sub(10,12);
+        Assert.assertEquals(result,-2);
+    }
+    @Test
+    void testDivision() {
+        ScientificCalculator sciCalObj = new ScientificCalculator();
+        int result = sciCalObj.div(10,5);
+        Assert.assertEquals(result,2);
+    }
+     @Test
+    void testMulti() {
+        ScientificCalculator sciCalObj = new ScientificCalculator();
+        int result = sciCalObj.mul(10,5);
+        Assert.assertEquals(result,50);
+    }
     
 }
