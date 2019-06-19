83a7d168f8c08e15415fe0c1d1a61b44dc71548b
PrashantPalhade <47560889+PrashantPalhade@users.noreply.github.com>
2019-06-19 19:23:47 +0530

    Update TestSciCalculator.java

--- seleniumtrainingshandsonartifacts/src/test/java/com/github/prashantpalhade/calculator/TestSciCalculator.java
+++ seleniumtrainingshandsonartifacts/src/test/java/com/github/prashantpalhade/calculator/TestSciCalculator.java
@@ -10,4 +10,12 @@ public class TestSciCalculator {
         int result = sciCalObj.sqrRoot(9);
 
         Assert.assertEquals(result,3);
+    }
+    @Test
+    void testcube() {
+        ScientificCalculator sciCalObj = new ScientificCalculator();
+        int result = sciCalObj.cube(9);
+        Assert.assertEquals(result,3);
+}
+    
 }
